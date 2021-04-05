bafae2510f0a5b55a1bfc33d256b9f450cc198ea
Paul Gardiner <paul@eoovi.com>
2021-04-05 22:48:14 +0100

    removed

--- test.js
+++
@@ -1,5 +0,0 @@
-const { Ittybit } = require('./lib');
-
-const ibit = new Ittybit({
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlhLWVJSMzZYTU9RVUlTSThMVU9IR0xZOUhEMjFRWlU0OSIsImxhYmVsIjoiZmFpbGVkLWJhc2tldC1oZWFkZWQtY29sdW1uLW1lZGljaW5lIiwiYWNjb3VudCI6IkVNQTZVUklBIiwiYXV0aG9yIjoiRFlIOVA2NkIiLCJpYXQiOjE2MTYwODE1MzZ9.ZWH_VNY9diakNM7pwHRqapL0Bc-SXJCpsLgAwNz2Hpw'
-});
