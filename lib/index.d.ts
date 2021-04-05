8e923549726c2de2ace2f94f386421e789502fe9
Paul Gardiner <paul@eoovi.com>
2021-04-05 15:22:35 +0100

    added simple gets videos/image/images/video

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,16 @@
-export declare class IttybitSDK {
-    constructor();
+interface Config {
+    token: string;
 }
+export declare class Ittybit {
+    config: Config;
+    private readonly apiurl;
+    private readonly apiport;
+    private readonly endpoint;
+    constructor(config: Config);
+    private request;
+    private get;
+    listImages(): Promise<any>;
+    listVideos(): Promise<any>;
+    getVideo(id: string): Promise<any>;
+}
+export {};
